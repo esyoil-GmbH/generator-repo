8ee324bdfc35995ae4254c8ee622d0f7ed60efce
Maurice B. Lewis <md@esyoil.com>
2019-05-03 09:19:47 +0000

    feat(storybook): update storybook to v5
    BREAKING CHANGE: Storybook was updated to v5.

--- configs/storybook/lib/config.js
+++ configs/storybook/lib/config.js
@@ -1,7 +1,8 @@
 import { configure } from "@storybook/react";
 
 function loadStories() {
-  require("../src/stories");
+  const req = require.context('../src/stories', true, /\.stories\.(js|ts)$/);
+  req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
