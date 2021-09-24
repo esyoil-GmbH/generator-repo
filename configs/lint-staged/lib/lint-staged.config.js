9dffd9f09e0ae7ec8a08d9d0aba22bdb13114ce1
SevenWaysEO <sergej.hoffmann@esyoil.com>
2021-09-24 11:08:51 +0200

    fix: add eslint for *.vue

--- configs/lint-staged/lib/lint-staged.config.js
+++ configs/lint-staged/lib/lint-staged.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "*.{,m}js": ["eslint --ignore-pattern '!*' --fix"],
     "*.md": ["prettier --write"],
     "*.ts{,x}": ["eslint --fix"],
+    "*.vue{,x}": ["eslint --fix"],
     "package{,-lock}.json": [
       "sort-package-json",
       "prettier --write"
